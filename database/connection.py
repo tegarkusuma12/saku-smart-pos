01c35b949d639a47a405155cec8b5115cdbbff57
tegarkusuma12 <kusumategar1255@gmail.com>
2026-09-13 14:45:50 +0700

    refactor: load db url from .env

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,8 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+load_dotenv() 
 
 # Default ke SQLite lokal untuk development
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///saku.db")
