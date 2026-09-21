ee6bbbb387169db7a9a5ca7167e3efcddca820f8
tegarkusuma12 <kusumategar1255@gmail.com>
2026-09-21 11:48:21 +0700

    feat: testing database integration

--- api/main.py
+++ api/main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from database.connection import get_db
+from database.models import Product
+
 
 app = FastAPI(
     title="SAKU Smart POS API",
     description="Backend API untuk SAKU Smart POS",
     version="1.0.0",
 )
+
+
+# ============================================================
+# BASIC
+# ============================================================
 
 @app.get("/")
 def root():
@@ -18,4 +28,24 @@ def root():
 def health_check():
     return {
         "status": "ok"
+    }
+
+
+# ============================================================
+# DATABASE TEST
+# ============================================================
+
+@app.get("/api/test-database")
+def test_database(db: Session = Depends(get_db)):
+    """
+    Endpoint sederhana untuk memastikan
+    FastAPI berhasil membaca database.
+    """
+
+    total_products = db.query(Product).count()
+
+    return {
+        "status": "success",
+        "database": "connected",
+        "total_products": total_products
     }
